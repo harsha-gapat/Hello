795b94200adf23c66a19b5cdc422362bed972567
sourabhkaleGC <113102589+sourabhkaleGC@users.noreply.github.com>
2022-09-13 10:05:07 +0530

    Create records.java
    Signed-off-by: sourabhkaleGC <113102589+sourabhkaleGC@users.noreply.github.com>

---
+++ Java/records.java
@@ -0,0 +1,25 @@
+public class Arguments {
+	public static void main(String[] args) throws Exception {
+		String dt=args[0];
+
+		SimpleDateFormat sdf = new SimpleDateFormat(dt);
+		Calendar cal = Calendar.getInstance();
+
+		java.util.Date date = cal.getTime();
+		
+		System.out.println("Last 10 Days Record");
+		
+		for (int i = 0; i < 10; i++) {
+			cal.add(Calendar.DATE, -1);
+		     date = cal.getTime();
+			String reportDate = sdf.format(date);
+
+			System.out.println(reportDate);
+		
+		
+		
+		
+	}
+
+}
+}
